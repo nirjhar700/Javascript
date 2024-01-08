de46fa51fa5079e2dab0fda55714255d5a5f333e
nirjhar700 <137003818+nirjhar700@users.noreply.github.com>
2024-01-08 21:30:28 +0000

    done

---
+++ Javascript/Scope.js
@@ -0,0 +1,21 @@
+// const webDev = [
+//     {
+//       markup: "Html",
+//       design: "Css"
+//     },
+//     {
+//       programming : "javascript",
+//       fastProgramming : "typescript"
+//     },
+//     {
+//       library : "React",
+//       framework : "Nextjs"
+//     }
+//   ]
+//   webDev.forEach((item)=>{
+//     console.log(item.markup);
+
+//   })
+  
+  
+  
